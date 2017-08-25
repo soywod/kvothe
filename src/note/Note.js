b547936c603ca4f5436800624e436781a5f4086a
soywod <clement.douin@gmail.com>
2017-08-25 08:31:06 +0200

    refactor(note): reduce nb files and export logic to repository

--- src/note/Note.js
+++ src/note/Note.js
@@ -1,56 +1,26 @@
 // @flow
 
-import type {NoteName, NoteAlt} from './Note.type';
+type NoteName = 'A' | 'B' | 'C' | 'D' | 'E' | 'F' | 'G';
+type NoteAlt = '♭' | '♯';
 
 type NoteParams = {
   name: NoteName;
-  alt: NoteAlt;
+  alt: ?NoteAlt;
 };
 
 class Note {
   id: string;
-  position: number;
   name: NoteName;
-  alt: NoteAlt;
+  alt: ?NoteAlt;
 
   constructor(params: NoteParams) {
-    const {name, alt} = params;
+    const {name, alt} = params
 
-    this.name = name;
-    this.alt = alt;
-
-    this.id = `${this.name}-${this.alt}`;
-    this.position = Note.getPositionById(this.id);
-  }
-
-  static getPositionById(id: string): number {
-    switch(id) {
-      case 'a-flat': return 0;
-      case 'g-sharp': return 0;
-      case 'a-natural': return 1;
-      case 'a-sharp': return 2;
-      case 'b-flat': return 2;
-      case 'b-natural': return 3;
-      case 'c-flat': return 3;
-      case 'b-sharp': return 4;
-      case 'c-natural': return 4;
-      case 'c-sharp': return 5;
-      case 'd-flat': return 5;
-      case 'd-natural': return 6;
-      case 'd-sharp': return 7;
-      case 'e-flat': return 7;
-      case 'e-natural': return 8;
-      case 'f-flat': return 8;
-      case 'e-sharp': return 9;
-      case 'f-natural': return 9;
-      case 'f-sharp': return 10;
-      case 'g-flat': return 10;
-      case 'g-natural': return 11;
-
-      default: return -1;
-    }
-  }
+    this.id = `${name}${alt || ''}`
+    this.name = name
+    this.alt = alt
+ }
 }
 
-export default Note;
-
+export type {NoteName, NoteAlt}
+export default Note
---
+++ src/note/NoteRepository.js
@@ -0,0 +1,111 @@
+// @flow
+
+import {assign, forEach, findKey} from 'lodash'
+
+import Note from './Note'
+import type {NoteName, NoteAlt} from './Note'
+
+type NoteMap = {
+  [id: string]: Note;
+}
+
+const NB_POSITIONS: number = 12
+
+class NoteRepository {
+  static instance: NoteRepository;
+  notes: NoteMap;
+
+  constructor() {
+    if(NoteRepository.instance instanceof NoteRepository) {
+      return NoteRepository.instance
+    }
+
+    const notes = {}
+
+    forEach(['A', 'B', 'C', 'D', 'E', 'F', 'G'], (name: NoteName) => {
+      forEach(['♭', '♯', null], (alt: ?NoteAlt) => {
+        const params = {name, alt}
+        const note = new Note(params)
+
+        notes[note.id] = note
+      })
+    })
+
+    this.notes = assign(this.notes, notes)
+    NoteRepository.instance = this
+  }
+
+  getById(id: string): ?Note {
+    return this.notes[id] || null
+  }
+
+  getByNameAndAlt(name: NoteName, alt: ?NoteAlt): ?Note {
+    const id = findKey(this.notes, (note: Note) => (
+      note.name === name && note.alt === alt
+    ))
+
+    if (! id) {
+      return null
+    }
+
+    return this.notes[id]
+  }
+
+  getNext(note: Note, offset: number = 1): Note {
+    const position = this.getPosition(note)
+    const nextPosition = (position + offset) % NB_POSITIONS
+    const id = findKey(this.notes, (currNote: Note) => (
+      this.getPosition(currNote) === nextPosition
+    ))
+    
+    if (! id) {
+      throw new Error('Next note not found')
+    }
+
+    return this.notes[id]
+  }
+
+  getTwin(note: Note): ?Note {
+    const position = this.getPosition(note)
+    const id = findKey(this.notes, (currNote: Note) => (
+      currNote.id !== note.id && this.getPosition(currNote) === position
+    ))
+
+    if (! id) {
+      return null
+    }
+
+    return this.notes[id]
+  }
+
+  getPosition(note: Note): ?number {
+    switch(note.id) {
+      case 'A♭': return 0
+      case 'G♯': return 0
+      case 'A': return 1
+      case 'A♯': return 2
+      case 'B♭': return 2
+      case 'B': return 3
+      case 'C♭': return 3
+      case 'B♯': return 4
+      case 'C': return 4
+      case 'C♯': return 5
+      case 'D♭': return 5
+      case 'D': return 6
+      case 'D♯': return 7
+      case 'E♭': return 7
+      case 'E': return 8
+      case 'F♭': return 8
+      case 'E♯': return 9
+      case 'F': return 9
+      case 'F♯': return 10
+      case 'G♭': return 10
+      case 'G': return 11
+
+      default: return null
+    }
+  }
+}
+
+export {NB_POSITIONS}
+export default NoteRepository
